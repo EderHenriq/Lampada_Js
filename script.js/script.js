b595a260282ba51dee753dfbfa3b928d563462a2
EderHenriq <eder.pontes2014@icloud.com>
2025-03-19 20:29:30 -0300

    Html e Java Script
    aula Mágica com lampadas

---
+++ script.js/script.js
@@ -0,0 +1,10 @@
+function Ligar(){
+    
+    document.getElementById('lampada') .src='imagens/lampada acessa.webp';
+
+}
+
+function Desligar(){
+    document.getElementById('lampada') .src='imagens/lamp-incand-ecologena-640a48d470a8e.webp';
+
+}
